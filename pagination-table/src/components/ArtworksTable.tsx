0c38714ced11ec0b3601e4fbecd0338193cf183f
nefcodes <nefcodes@gmail.com>
2024-09-13 11:04:44 +0530

    debugging

--- pagination-table/src/components/ArtworksTable.tsx
+++ pagination-table/src/components/ArtworksTable.tsx
@@ -9,6 +9,7 @@ const ArtworksTable: React.FC = () => {
   const [totalRecords, setTotalRecords] = useState(0);
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
+  const rows = 6;
 
   // Get the current page from the query params
   const currentPage = Number(searchParams.get('page')) || 1;
@@ -20,6 +21,9 @@ const ArtworksTable: React.FC = () => {
       .then((data) => {
         setArtworks(data.data);
         setTotalRecords(data.pagination.total);
+        console.log("------------>");
+        console.log(data.pagination.total);
+        console.log("------------>");
         setLoading(false);
       });
   };
@@ -29,17 +33,19 @@ const ArtworksTable: React.FC = () => {
   }, [currentPage]);
 
   const onPageChange = (event: any) => {
-    const newPage = event.page + 1;
-    setSearchParams({ page: newPage.toString() });
+    // const newPage = event.page + 1;
+    // setSearchParams({ page: newPage.toString() });
+    console.log(event);
+    
   };
 
   return (
-    <div>
+    <div className='artworks'>
       <h2>Artworks Table</h2>
       <DataTable value={artworks} loading={loading} paginator
-                 rows={10} totalRecords={totalRecords}
-                 first={(currentPage - 1) * 10}
-                 onPage={onPageChange}>
+        rows={rows} totalRecords={totalRecords}
+        first={(currentPage - 1) * 10}
+        onPage={onPageChange}>
         <Column field="title" header="Title" />
         <Column field="place_of_origin" header="Place of Origin" />
         <Column field="artist_display" header="Artist" />
